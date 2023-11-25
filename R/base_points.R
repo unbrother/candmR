fa03b0d10872f0fc5ea389e6fee2a4bb4e1c1910
unbrother <mendozajose@outlook.com>
2023-11-25 04:01:47 -0600

    Times vignette complete

---
+++ R/base_points.R
@@ -0,0 +1,18 @@
+#' Example base waypoints for TT Methods
+#'
+#' Dataframe containing waypoints for two routes (one by direction) of an
+#' example route to gather travel times
+
+#' @format Dataframe with 24 observations of 5 variables
+
+#' \describe{
+#' \item{Name}{Name of the given waypoint}
+#' \item{Description}{Description field used by Google Earth (not used)}
+#' \item{Route}{Route identifier}
+#' \item{Point}{Point number within route}
+#' \item{geometry}{Special Features geometry column}
+#' }
+
+#' @examples
+#' base_points
+"base_points"
---
+++ R/travel_times.R
@@ -0,0 +1,30 @@
+#' Example of travel times query results
+#'
+#' Dataframe containing resulting queries from travel times example
+
+#' @format Dataframe with 506 observations of 18 variables
+
+#' \describe{
+#' \item{alternative_id}{}
+#' \item{leg_id}{}
+#' \item{summary}{}
+#' \item{distance_m}{}
+#' \item{distance_text}{}
+#' \item{duration_s}{}
+#' \item{duration_text}{}
+#' \item{duration_in_traffic_s}{}
+#' \item{duration_in_traffic_text}{}
+#' \item{departure_time}{}
+#' \item{arrival_time}{}
+#' \item{TOD}{}
+#' \item{route}{}
+#' \item{Code}{}
+#' \item{geometry}{}
+#' \item{travel_time}{}
+#' \item{distance}{}
+#' \item{speed}{}
+#' }
+
+#' @examples
+#' travel_times
+"travel_times"
